985cd595211734559b7b0bd64a9783ff6128e15b
José Manuel Ortiz Sánchez <39153780+josemortizs@users.noreply.github.com>
2021-08-09 22:03:54 +0200

    Initial Commit

---
+++ WorkWithLists/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkWithLists
+//
+//  Created by Jose Manuel Ortiz Sanchez on 9/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorkWithLists/WorkWithListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkWithListsApp.swift
+//  WorkWithLists
+//
+//  Created by Jose Manuel Ortiz Sanchez on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct WorkWithListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
