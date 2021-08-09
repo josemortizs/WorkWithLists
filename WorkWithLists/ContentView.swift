b732044d78e409d26890f796417850e2f0d057f6
José Manuel Ortiz Sánchez <39153780+josemortizs@users.noreply.github.com>
2021-08-09 22:23:53 +0200

    Show the data, contained in an array, using a list and a foreach

--- WorkWithLists/ContentView.swift
+++ WorkWithLists/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var devs = [
+        FamousDevelopers(id: 0, name: "Chris Lattner", language: "Swift"),
+        FamousDevelopers(id: 1, name: "Brad Cox", language: "Objective-C"),
+        FamousDevelopers(id: 2, name: "Dennis Ritchie", language: "C"),
+        FamousDevelopers(id: 3, name: "James Gosling", language: "Java"),
+        FamousDevelopers(id: 4, name: "Bjarne Stroustrup", language: "C++"),
+        FamousDevelopers(id: 5, name: "Guido van Rossum", language: "Python"),
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            ForEach(devs) { dev in
+                DevView(dev: dev)
+            }
+        }.padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct FamousDevelopers: Identifiable {
+    let id: Int
+    let name: String
+    let language: String
+}
+
+struct DevView: View {
+    
+    var dev: FamousDevelopers
+    
+    var body: some View {
+        HStack {
+            Text(dev.name)
+            Spacer()
+            Text(dev.language)
+        }.padding()
     }
 }
